570a889755aa54608b851beefe4d4a18d9a313d1
Paul Baliasny <leave.before.dawn@gmail.com>
2021-08-22 13:51:49 +0300

    Fix: bugs fixed after reviewer's comments

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ let editButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let closeButton = document.querySelector(".popup__closer");
 let form = document.querySelector(".popup__form");
-let inputName = document.querySelector(".popup__name");
-let inputJob = document.querySelector(".popup__profession");
+let inputName = document.querySelector(".popup__input_text_name");
+let inputJob = document.querySelector(".popup__input_text_profession");
 let userName = document.querySelector(".profile__name");
 let userJob = document.querySelector(".profile__about");
 
-function closePopup() {
-    popup.classList.remove("popup_active");
-}
-
 editButton.addEventListener("click", function() {
     popup.classList.add("popup_active");
+    inputName.value = userName.textContent;
+    inputJob.value = userJob.textContent;
 })
 
 closeButton.addEventListener("click", closePopup);
@@ -25,3 +23,7 @@ form.addEventListener("submit", function(e) {
     userJob.textContent = jobValue;
     closePopup();
 })
+
+function closePopup() {
+    popup.classList.remove("popup_active");
+}
