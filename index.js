c7fbfa6de6217fa37c546ea5033c75664decdb72
Paul Baliasny <leave.before.dawn@gmail.com>
2021-08-28 17:33:25 +0300

    Add interactivity features project 5

--- index.js
+++ index.js
@@ -1,29 +1,163 @@
-let editButton = document.querySelector(".profile__edit-button");
-let popup = document.querySelector(".popup");
-let closeButton = document.querySelector(".popup__closer");
-let form = document.querySelector(".popup__form");
-let inputName = document.querySelector(".popup__input_text_name");
-let inputJob = document.querySelector(".popup__input_text_profession");
-let userName = document.querySelector(".profile__name");
-let userJob = document.querySelector(".profile__about");
+// pop-ups
+
+const editProfilePopup = document.querySelector(".popup_type_edit");
+const addPlacePopup = document.querySelector(".popup_type_add");
+const picturePopup = document.querySelector(".popup_type_picture");
+
+// pop-up opening buttons
+
+const editButton = document.querySelector(".profile__edit-button");
+const addButton = document.querySelector(".profile__add-button");
+
+// pop-up closing buttons
+
+const editProfileCloseButton = editProfilePopup.querySelector(".popup__closer");
+const addCardCloseButton = addPlacePopup.querySelector(".popup__closer");
+const picturePopupCloseButton = picturePopup.querySelector(".popup__closer");
+
+// pop-up image
+
+const picturePopupImage = picturePopup.querySelector(".popup__image");
+const picturePopupTitle = picturePopup.querySelector(".popup__image-title");
+
+
+// input fields
+const inputName = editProfilePopup.querySelector(".popup__input_text_name");
+const inputJob = editProfilePopup.querySelector(".popup__input_text_profession");
+const userName = document.querySelector(".profile__name");
+const userJob = document.querySelector(".profile__about");
+const inputPlaceName = addPlacePopup.querySelector(".popup__input_text_toponym");
+const inputPlaceLink = addPlacePopup.querySelector(".popup__input_text_link");
+const formEditProfile = editProfilePopup.querySelector(".popup__form");
+const formAddPlace = addPlacePopup.querySelector(".popup__form");
+
+function togglePopup(popup) {
+    popup.classList.toggle("popup_active");
+}
+// open buttons
 
 editButton.addEventListener("click", function() {
-    popup.classList.add("popup_active");
+    togglePopup(editProfilePopup);
     inputName.value = userName.textContent;
     inputJob.value = userJob.textContent;
 })
 
-closeButton.addEventListener("click", closePopup);
+addButton.addEventListener("click", function() {
+    formAddPlace.reset();
+    togglePopup(addPlacePopup);
+});
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    let nameValue = inputName.value;
-    let jobValue = inputJob.value;
-    userName.textContent = nameValue;
-    userJob.textContent = jobValue;
-    closePopup();
+// close buttons
+
+editProfileCloseButton.addEventListener("click", () => {
+    togglePopup(editProfilePopup);
+});
+
+addCardCloseButton.addEventListener("click", () => {
+    togglePopup(addPlacePopup);
+});
+
+picturePopupCloseButton.addEventListener("click", () => {
+    togglePopup(picturePopup);
 })
 
-function closePopup() {
-    popup.classList.remove("popup_active");
-}
+formEditProfile.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    const nameValue = inputName.value;
+    const jobValue = inputJob.value;
+    userName.textContent = nameValue;
+    userJob.textContent = jobValue;
+    togglePopup(editProfilePopup);
+});
+
+formAddPlace.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    createPlaceCard({ name: inputPlaceName.value, link: inputPlaceLink.value });
+    togglePopup(addPlacePopup);
+});
+
+const initialCards = [{
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
+
+const templatePlace = document.querySelector("#place-template").content.querySelector(".place");
+const placesSection = document.querySelector(".places");
+
+
+
+function initialPlaces(placeInfo) {
+    const place = templatePlace.cloneNode(true);
+    const placeTitle = place.querySelector(".place__title");
+    const placeImage = place.querySelector(".place__image");
+    const deleteButton = place.querySelector(".place__delete");
+    const likeButton = place.querySelector(".place__like");
+
+    placeTitle.textContent = placeInfo.name;
+    placeImage.style.backgroundImage = `url(${placeInfo.link})`;
+
+
+    likeButton.addEventListener("click", () => {
+        likeButton.classList.toggle("place__like_clicked");
+    });
+
+    deleteButton.addEventListener("click", () => {
+        place.remove();
+    });
+
+    placeImage.addEventListener("click", () => {
+        picturePopupImage.src = placeInfo.link;
+        picturePopupTitle.textContent = placeInfo.name;
+        togglePopup(picturePopup);
+    });
+    placesSection.append(place);
+};
+
+initialCards.forEach(initialPlaces);
+
+function createPlaceCard(placeInfo) {
+    const place = templatePlace.cloneNode(true);
+    const placeTitle = place.querySelector(".place__title");
+    const placeImage = place.querySelector(".place__image");
+    const deleteButton = place.querySelector(".place__delete");
+    const likeButton = place.querySelector(".place__like");
+    placeTitle.textContent = placeInfo.name;
+    placeImage.style.backgroundImage = `url(${placeInfo.link})`;
+
+    likeButton.addEventListener("click", () => {
+        likeButton.classList.toggle("place__like_clicked");
+    });
+
+    deleteButton.addEventListener("click", () => {
+        place.remove();
+    });
+
+    placeImage.addEventListener("click", () => {
+        picturePopupImage.src = placeInfo.link;
+        picturePopupTitle.textContent = placeInfo.name;
+        togglePopup(picturePopup);
+    });
+
+    placesSection.prepend(place);
+};
